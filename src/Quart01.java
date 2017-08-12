232bac40602367eae95934b8c00e996852ed0ad1
StevenZhao13 <zhaoyuzhengze@gmail.com>
2017-08-12 02:29:12 -0500

    another daily bit

--- src/Quart01.java
+++ src/Quart01.java
@@ -3,11 +3,11 @@ public class Quart01 {
 	// 1. Two Sum
 	public int[] twoSum(int[] nums, int target) {
 		int[] ret= new int[2];
-		
+
 		for (int i = 0; i<nums.length; i++){
 			for (int j = 0; j<nums.length; j++){
 				if (i != j){
-				    if (nums[i]+nums[j]==target){
+					if (nums[i]+nums[j]==target){
 						ret[0] = i;
 						ret[1] = j;
 						return ret;
@@ -15,9 +15,63 @@ public class Quart01 {
 				}
 			}
 		}
-
-
 		return null;
 	}
+
+
+	// 2. Add Two Number 
+	/**
+	 * Definition for singly-linked list.
+	 */
+	public class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) { val = x; }
+	}
+
+
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		if ( l1.val == 0 && l2.val == 0 
+				&& l1.next == null && l2.next == null){
+			return new ListNode(0);
+		}
+
+		return addHelper(l1,l2,0);
+
+	}
+
+	public ListNode addHelper(ListNode l1, ListNode l2, int increment){
+		int value = 0 + increment;
+
+		if(l1 != null)		value += l1.val;
+		if(l2 != null)		value += l2.val;
+
+		if (value == 0
+				&& (l1 == null || l1.next == null )
+				&& (l2 == null || l2.next == null )){	
+			return null;
+		} else{
+			int nextIncrement = 0;
+			if (value >= 10){
+				nextIncrement = 1;
+				value = value - 10;
+			} else {}
+
+			ListNode ret = new ListNode(value);
+			ret.next = addHelper(
+					(l1==null) ? null : l1.next,
+					(l2==null) ? null : l2.next,
+					nextIncrement);
+
+			return ret;
+		}
+
+	}
+	
+	
+	
+	
+	
+	
 
 }
