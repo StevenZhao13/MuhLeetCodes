75c330c5dcb0de80f14ac8da9d4b8593d64f9b53
StevenZhao13 <zhaoyuzhengze@gmail.com>
2017-08-13 14:44:09 -0500

    daily bit of algorithm

--- src/Quart01.java
+++ src/Quart01.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 
 public class Quart01 {
 	// 1. Two Sum
@@ -69,6 +71,22 @@ public class Quart01 {
 	}
 	
 	
+	
+	// 2. Longest Substring Without Repeating Characters 
+    public int lengthOfLongestSubstring(String s) {
+		if (s.length()==0) return 0;
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		int max=0;
+		for (int i=0, j=0; i<s.length(); ++i){
+			if (map.containsKey(s.charAt(i))){
+				j = Math.max(j,map.get(s.charAt(i))+1);
+			}
+			map.put(s.charAt(i),i);
+			max = Math.max(max,i-j+1);
+		}
+		return max;
+}
+
 	
 	
 	
