020e960ae3dc79f03d1eb9996609672d37351208
StevenZhao13 <zhaoyuzhengze@gmail.com>
2017-08-25 18:22:03 -0500

    daily bit

--- src/Quart01.java
+++ src/Quart01.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.List;
@@ -226,14 +227,14 @@ public class Quart01 {
 
 
 	// 20. parenthesis paring validation
-    public boolean isValid(String s) {
-    	Stack<Character> stack = new Stack<Character>();
-    	Hashtable<Character, Character> ht = new Hashtable<Character, Character>();
-    	ht.put((char)41,(char)40);
-    	ht.put((char)93,(char)91);
-    	ht.put((char)125,(char)123);
+	public boolean isValid(String s) {
+		Stack<Character> stack = new Stack<Character>();
+		Hashtable<Character, Character> ht = new Hashtable<Character, Character>();
+		ht.put((char)41,(char)40);
+		ht.put((char)93,(char)91);
+		ht.put((char)125,(char)123);
 
-    	
+
 		for (int i = 0; i< s.length(); i++){
 			if (s.charAt(i) == 40 || s.charAt(i) == 91 || s.charAt(i) == 123){
 				stack.push(s.charAt(i));
@@ -246,14 +247,14 @@ public class Quart01 {
 				}
 			}
 		}
-    	
-    	return stack.isEmpty();
 
-    }
-    
-    
-    // 21. Merging sorted list
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		return stack.isEmpty();
+
+	}
+
+
+	// 21. Merging sorted list
+	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 		if(l1 == null) return l2;
 		if(l2 == null) return l1;
 		if(l1.val < l2.val){
@@ -263,32 +264,83 @@ public class Quart01 {
 			l2.next = mergeTwoLists(l1, l2.next);
 			return l2;
 		}
-    }
-    
-    // 22. generate parenthesis
-    public List<String> generateParenthesis(int n) {
-        List<String> list = new ArrayList<String>();
-        backtrack(list, "", 0, 0, n);
-        return list;
-    }
-    
-    public void backtrack(List<String> list, String str, int open, int close, int max){
-        
-        if(str.length() == max*2){
-            list.add(str);
-            return;
-        }
-        
-        if(open < max)
-            backtrack(list, str+"(", open+1, close, max);
-        if(close < open)
-            backtrack(list, str+")", open, close+1, max);
-    }
+	}
+
+	// 22. generate parenthesis
+	public List<String> generateParenthesis(int n) {
+		List<String> list = new ArrayList<String>();
+		backtrack(list, "", 0, 0, n);
+		return list;
+	}
+
+	public void backtrack(List<String> list, String str, int open, int close, int max){
+
+		if(str.length() == max*2){
+			list.add(str);
+			return;
+		}
+
+		if(open < max)
+			backtrack(list, str+"(", open+1, close, max);
+		if(close < open)
+			backtrack(list, str+")", open, close+1, max);
+	}
+
+
+	// 23. Merge k Sorted Linked List
+	public ListNode mergeKLists(ListNode[] lists) {
+		ListNode returnHeadNode = null;
+
+		ListNode lastNode = null;
 
 
 
-    
-    
+		boolean listEmptied = false;
+
+		while (!listEmptied){
+
+			int smallestIndex = findSmallestNodeIndex(lists);
+
+			if (smallestIndex == -1){
+				return null;
+
+			} else {
+				
+				ListNode newNode = new ListNode(lists[smallestIndex].val);
+				
+				lists[smallestIndex] = lists[smallestIndex].next;
+				
+				if (returnHeadNode == null) {
+					returnHeadNode = newNode;
+					lastNode = returnHeadNode;
+				} else {
+					lastNode.next = newNode;
+					lastNode = newNode;
+				}
+				
+			}
+		}
+
+		return returnHeadNode;
+	}
+
+	public int findSmallestNodeIndex(ListNode[] lists){
+		int smallestIndex = -1;					// if the list is empty, it will stay be -1
+		int smallestNo = Integer.MAX_VALUE;
+
+		for (int i = 0; i < lists.length; i++){
+
+			if (		lists[i] != null
+					&& 	lists[i].val < smallestNo){
+				smallestNo = lists[i].val;
+				smallestIndex = i;
+			} else {}
+		}
+
+		return smallestIndex;
+	}
+
+
 }
 
 
